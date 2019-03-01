8192420f5f712faf19b89fbc4faf71aacd6b8e7d
mini.robot <mini.uc3m@gmail.com>
2019-03-01 13:50:38 +0100

    backup

--- xml/xml_reader.py
+++ xml/xml_reader.py
@@ -2,13 +2,14 @@
 # +*+ coding: utf+8 +*+
 
 import xml.etree.ElementTree as ET 			# To read xml file (questions and sentences)
+from ca_functions import *
 
-def GetQuestion(question_type):
+def Test(question_type):
 	"""
-	@brief: Get a question from 'bingo_question.xml'
+	@brief: Get a question from 'skill_question.xml'
 	@param string question_type: kind of question
 	"""
-	tree = ET.parse('bingo_questions.xml')			# Read xml file
+	tree = ET.parse('skill_questions.xml')			# Read xml file
 	root = tree.getroot()										# Make a tree with the data
 	#xml_path_full = "question/[@type='" + question_type + "']/etts/[@type='en']"		# Make xml path of the type
 	xml_path_full = "question/[@type='" + question_type + "']/etts"		# Make xml path of the type
@@ -18,17 +19,36 @@ def GetQuestion(question_type):
 
 	# Direct search
 	etts = root.find(xml_path_full).text				# Save the data: question, grammar and answer_id
-	print etts
+	print(etts)
 
 	# By parts search
 	question = root.find(xml_path_question)						# Find the question type
 	etts = question.find(xml_path_etts).text				# Save the data: question, grammar and answer_id
 	#etts = etts.replace('%name', 'Pepe')		# Introduce the user's name
-	print etts
+	print(etts)
 	grammar = question.find('grammar').text
 	answer_id = question.find('answer_id').text
 
 	print ('Question: %s\nGrammar: %s\nAnswer id: %s' % (etts.encode("utf-8"), grammar, answer_id))					
-	print etts
+	print(etts)
 
-GetQuestion('choose_card')
+def GetQuestion(question_type, language):
+	"""
+	@brief: Get a question from 'skill_question.xml'
+	@param string question_type: kind of question
+	"""
+	tree = ET.parse('skill_questions.xml') # Read xml file
+	root = tree.getroot() # Make a tree with the data
+	question_info_path = "skill_questions/question/[@type='" + question_type + "']/language"
+
+	# Save data
+	question = root.find(question_info_path) # Find the question type
+	print question.text
+	etts = question.find('etts').text
+	etts = etts.encode("utf-8")
+	grammar = question.find('grammar').text
+	answer_id = question.find('answer_id').text
+
+	print ('Question: %s\nGrammar: %s\nAnswer id: %s' % (etts, grammar, answer_id))					
+
+GetQuestion('ask_to_continue', 'es')
